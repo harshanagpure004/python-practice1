58b0e9cd9e6d0b0564400dccaf988ab742fca384
Harsha Nagpure <harshanagpure04@gmail.com>
2026-01-21 13:55:59 +0530

    Add files via upload

---
+++ 02_prac_01_twinkle.py
@@ -0,0 +1,7 @@
+#Que.1)  write a program to print twinkle twinkle little star poem in python.
+#Solution:
+
+print('''Twinkle, twinkle, little star,
+How I wonder what you are!
+Up above the world so high,
+Like a diamond in the sky.''') # we use '''.....''' for printing multiple line of code at one time
