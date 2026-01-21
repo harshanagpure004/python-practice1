993607ac141524ff61c165d6700ee5df3e0623ae
Harsha Nagpure <harshanagpure04@gmail.com>
2026-01-21 15:51:33 +0530

    music_play.py
    # Python Music Player Using Playsound
    
    This is a simple Python program that plays a music file using the external `playsound` module.  
    
    ## How to run:
    1. Install the module by running `pip install playsound` in your terminal.  
    2. Change the file path in the code to the location of your MP3 music file.  
    3. Run the program, and it will play your music automatically.
    
    ---
    
    *This project helped me learn how to use external Python modules and play audio files from a script.*

---
+++ 03_Prac_soundmodule_usage.py
@@ -0,0 +1,8 @@
+# write an python program --install an external module and use it to perform an operation of your interest.
+#Solution:
+
+from playsound import playsound
+playsound('c:\\Users\\Computer HuB\\Downloads\\Dooron Dooron play.mp3') 
+''' search playsound= copy=open new terminal =paste it =install pip install playsound=write python code=
+=write mp3 music path =run'''
+
